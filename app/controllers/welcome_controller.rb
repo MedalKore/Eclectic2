c992d920934b0c84d74ef319b3e71d99e9b74811
Andrew Lemmons <andrew@medalkore.com>
2012-11-09 00:19:28 -0600

    sometihgn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,8 +22,8 @@ class WelcomeController < ApplicationController
   			flash[:error] = @prospect.errors.full_messages
   			format.html {redirect_to :error}
   		else
-        gib = Gibbon.new('a7dc350137ef0fab605f124ac5d95646-us6')
-        gib.list_subscribe(:id => '5fc70fd929', :email_address => @email, :merge_vars => {:FNAME => @first_name})
+        # gib = Gibbon.new('a7dc350137ef0fab605f124ac5d95646-us6')
+        # gib.list_subscribe(:id => '5fc70fd929', :email_address => @email, :merge_vars => {:FNAME => @first_name})
   			flash[:notice] = 'Submission successful. Thank You! Please check your email for confirmation.'
   			format.html {redirect_to :thankyou}
   		end
